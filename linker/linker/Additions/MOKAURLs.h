4c46249802c87204c05571db5cb9ec3062ce5785
李迪 <284289438@qq.com>
2014-09-13 16:24:01 +0800

    333333333333

--- linker/linker/Additions/MOKAURLs.h
+++ linker/linker/Additions/MOKAURLs.h
@@ -49,6 +49,6 @@
 
 #define URL_SUB_CREATEORDER [URL_BASE stringByAppendingString:@"apicreateorder"]
 
-#define URL_SUB_REFUNDERLIST [URL_BASE stringByAppendingString:@"getrefunderlist"]
+#define URL_SUB_REFUNDERLIST [URL_BASE stringByAppendingString:@"apigetrefunderlist"]
 
 #endif
